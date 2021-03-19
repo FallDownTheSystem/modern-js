dabe78296197ef4d673f773f6865586db3d51caa
FallDown <8807171+FallDownTheSystem@users.noreply.github.com>
2021-03-20 01:59:25 +0200

    Embed

---
+++ src/pages/embed.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="w-full px-48 py-8">
+		<div class="shadow-md">
+			<iframe
+				src="https://codesandbox.io/embed/blissful-buck-ry3uz?fontsize=14&hidenavigation=1&module=%2Fsrc%2FApp.vue&moduleview=1&theme=dark"
+				style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+				title="Vue 3 Vite Starter"
+				allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+				sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+				class="shadow-2xl"
+			></iframe>
+		</div>
+	</div>
+</template>
