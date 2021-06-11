e1d727ff2121b4e9fde0e56bb46810ba78128876
FallDown <8807171+FallDownTheSystem@users.noreply.github.com>
2021-06-11 19:08:38 +0300

    Fix next article navigation

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,35 +1,49 @@
 <template>
 	<SiteHeader />
-	<Transition appear enter-from-class="opacity-0 -translate-x-64" enter-active-class="delay-200" enter-to-class="opacity-100 translate-x-0">
+	<Transition
+		appear
+		enter-from-class="opacity-0 -translate-x-64"
+		enter-active-class="delay-200"
+		enter-to-class="opacity-100 translate-x-0"
+	>
 		<nav
-			class="transition-all transform duration-300 hidden full:flex justify-center fixed text-white pl-4 top-1/2 transform -translate-y-1/2"
+			class="text-white transform pl-4 transition-all top-1/2 -translate-y-1/2 duration-300 hidden full:flex justify-center fixed"
 			aria-label="Progress"
 		>
 			<ol class="space-y-4">
 				<li>
-					<router-link :to="{ name: 'index' }" class="flex items-center group outline-none" aria-current="step">
-						<i-tabler:arrow-narrow-left class="w-5 h-5 text-gray-500 group-hover:text-gray-300 group-focus:text-gray-300" />
-						<span class="ml-3 text-sm font-medium truncate w-56 text-gray-500 group-hover:text-gray-300 group-focus:text-gray-300">
-							Home
-						</span>
+					<router-link
+						:to="{ name: 'index' }"
+						class="flex outline-none items-center group"
+						aria-current="step"
+					>
+						<i-tabler:arrow-narrow-left
+							class="h-5 text-gray-500 w-5 group-hover:text-gray-300 group-focus:text-gray-300"
+						/>
+						<span
+							class="font-medium text-sm ml-3 text-gray-500 w-56 truncate group-hover:text-gray-300 group-focus:text-gray-300"
+						>Home</span>
 					</router-link>
 				</li>
 				<li v-for="h in headings" :key="h.hash">
 					<!-- Current Step -->
 					<a
 						:href="h.hash"
-						class="flex items-center group outline-none"
+						class="flex outline-none items-center group"
 						:class="{ 'ml-3': h.type == 'H3', 'ml-6': h.type == 'H4' }"
 						aria-current="step"
 					>
-						<span class="flex-shrink-0 h-5 w-5 relative flex items-center justify-center" aria-hidden="true">
+						<span
+							class="flex flex-shrink-0 h-5 w-5 relative items-center justify-center"
+							aria-hidden="true"
+						>
 							<span
 								v-if="current == h.hash"
-								class="absolute rounded-full bg-pink-900"
+								class="rounded-full bg-pink-900 absolute"
 								:class="{ 'h-4 w-4': h.type == 'H2' || h.type == 'H1', 'h-3 w-3': h.type == 'H3', 'h-2 w-2': h.type == 'H4' }"
 							></span>
 							<span
-								class="relative block rounded-full"
+								class="rounded-full relative block"
 								:class="{
 									'w-2 h-2': h.type == 'H2' || h.type == 'H1',
 									'w-1.5 h-1.5': h.type == 'H3',
@@ -40,14 +54,12 @@
 							></span>
 						</span>
 						<span
-							class="ml-3 text-sm font-medium truncate w-56"
+							class="font-medium text-sm ml-3 w-56 truncate"
 							:class="{
 								'text-pink-600 group-focus:underline': current == h.hash,
 								'text-gray-500 group-hover:text-gray-300 group-focus:text-gray-300': current != h.hash
 							}"
-						>
-							{{ h.title }}
-						</span>
+						>{{ h.title }}</span>
 					</a>
 				</li>
 			</ol>
@@ -89,7 +101,7 @@ const handleMounted = () => {
 	// Bottom nav
 	const currentRoute = route.matched.find(x => x.path !== '/');
 	const articles = sortBy(
-		router.getRoutes().filter(x => x.path !== '/' && x.name !== 'all'),
+		router.getRoutes().filter(x => x.path !== '/' && x.name !== 'all' && x.name),
 		'meta.order'
 	);
 	if (articles && currentRoute) {
@@ -100,7 +112,7 @@ const handleMounted = () => {
 };
 
 onMounted(() => {
-	document.addEventListener('scroll', function (e) {
+	document.addEventListener('scroll', function(e) {
 		for (const heading of headings) {
 			if (heading.pos < window.scrollY + window.innerHeight / 2.1) {
 				current = heading.hash;
